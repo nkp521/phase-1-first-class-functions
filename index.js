a2ec0ce7480ca40b25dd2a0dc9a867345e12c69c
Nihar Patel <niharpatel52@gmail.com>
2025-05-07 09:52:39 -0400

    Complete Lab

---
+++ index.js
@@ -0,0 +1,13 @@
+function receivesAFunction(cb) {
+  cb();
+}
+
+function returnsANamedFunction(){
+  return receivesAFunction;
+}
+
+function returnsAnAnonymousFunction() {
+  return function() {
+    console.log(Hello);
+  }
+}
